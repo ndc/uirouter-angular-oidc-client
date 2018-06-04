ddb83bc64f4070ec9a9ef8a43527b91e3bf5f246
Endy Tjahjono <endy.tjahjono@verint.com>
2018-06-04 18:59:19 +0700

    Added route to handle OP redirect. Added feature to remember the original target before authenticating. Fixed how to redirect to another route. Added comments explaining things.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,9 +13,32 @@ const rootModule: RootModule = {
     },
     {
       name: "OIDCRedirect",
-      url: "/oidcredirect",
+      url: "/oidcredirect/*oidcvalues",
+      resolve: [
+        {
+          token: "OidcUser",
+          deps: [Transition, VeApiService],
+          resolveFn: (trans: Transition, api: VeApiService) => {
+            // capture hash fragment from OP
+            let valueStr: string = trans.params().oidcvalues;
+
+            return api.ProcessOidcRedirect(valueStr);
+          }
+        }
+      ],
       onEnter: (trans: Transition, state: StateDeclaration) => {
-        trans.router.stateService.go("Home");
+        // wait for oidc-client to finish processing OP's hash fragment
+        return trans.injector().getAsync("OidcUser").then(user => {
+          // recall the original target before user is asked to authenticate
+          let originalTarget = JSON.parse(sessionStorage.getItem("OriginalTarget"));
+          let target = trans.router.stateService.target(originalTarget.name, originalTarget.params);
+
+          // clean up temporary storage
+          sessionStorage.removeItem("OriginalTarget");
+
+          // redirect to original target
+          return target;
+        });
       }
     },
     {
@@ -44,40 +67,58 @@ const rootModule: RootModule = {
     uiRouter.urlService.rules.otherwise(otherwiseHandler);
 
     let api = injector.get(VeApiService);
-    InitializeOidc(api).then(() => {
+
+    // if OP doesn't mangle the redirect URL that we gave it, we can capture inside the routing system
+    let redirectUriUsingRouting = window.location.href.split("#")[0] + "#/oidcredirect/";
+
+    // if OP mangles the redirect URL that we gave it, we need a separate HTML page to capture the hash fragment
+    let redirectUriUsingOutside = window.location.href.split("#")[0] + "assets/oidcredirect.html";
+
+    // provide settings to oidc-client and ask it to read existing authentication if available
+    InitializeOidc(api, redirectUriUsingRouting).then(() => {
+      // adding authorization guard must be done after oidc-client is ready
       requiresAuthHook(uiRouter.transitionService, api, uiRouter.stateService);
     });
   },
   useHash: true
 };
 
-export function InitializeOidc(api: VeApiService) {
-  let redirectUri = window.location.href.split("#")[0]
-    + "assets/oidcredirect.html";
+export function InitializeOidc(api: VeApiService, oidcRedirectUri: string) {
   return api.InitializeOidc({
     authority: "https://demo.identityserver.io",
     client_id: "implicit",
-    redirect_uri: redirectUri,
+    redirect_uri: oidcRedirectUri,
     response_type: "id_token token",
     scope: "openid profile email api"
   });
 }
 
 export function requiresAuthHook(trans: TransitionService, api: VeApiService, state: StateService) {
+  // if a route has 'data' and the data contains requiresAuth that is truthy ...
   const requiresAuthCriteria: HookMatchCriteria = {
     to: (state) => !!state.data && !!state.data.requiresAuth
   };
 
+  // ... then check if user is logged in. If not ...
   const redirectToLogin: TransitionHookFn = (transition) => {
     if (!api.IsLoggedIn()) {
-      let target = state.href(
-        transition.targetState().name(),
-        transition.targetState().params(),
-        { absolute: true });
-      return api.StartSignIn(target);
+      // memorize original target in sessionStorage before asking user to authenticate
+      let originalTarget = {
+        name: transition.targetState().name(),
+        params: transition.targetState().params()
+      }
+      let originalTargetStr = JSON.stringify(originalTarget);
+      sessionStorage.setItem("OriginalTarget", originalTargetStr);
+
+      // redirect to login page
+      return api.StartSignInWithRedirect();
+
+      // or use pop up to login
+      return api.StartSignInWithPopUp();
     }
   };
 
+  // activate this rule
   trans.onBefore(requiresAuthCriteria, redirectToLogin, { priority: 10 });
 }
 
--- src/app/ve-api/ve-api.service.ts
+++ src/app/ve-api/ve-api.service.ts
@@ -17,14 +17,20 @@ export class VeApiService {
   InitializeOidc(settings: UserManagerSettings) {
     this.OidcManager = new UserManager(settings);
 
+    // if login using redirect, check sessionStorage for value and process if available
     let signInRedirect = sessionStorage.getItem("OidcRedirect");
+
     if (!!signInRedirect) {
+      // process hash fragment from OP saved in sessionStorage
       return this.OidcManager.signinRedirectCallback(signInRedirect).then(user => {
         this.OidcUser = user;
+
+        // clean up
         sessionStorage.removeItem("OidcRedirect");
         this.OidcManager.clearStaleState();
       });
     } else {
+      // check existing authentication if available (oidc-client uses sessionStorage)
       return this.OidcManager.getUser().then(user => {
         if (!!user) {
           this.OidcUser = user;
@@ -41,12 +47,22 @@ export class VeApiService {
     return this.OidcUser;
   }
 
-  StartSignIn(target: string) {
+  // if OP doesn't mangle redirect URL and we are using routing system to capture hash fragment so not using sessionStorage
+  ProcessOidcRedirect(valueStr: string) {
+    return this.OidcManager.signinRedirectCallback(valueStr).then(user => {
+      this.OidcUser = user;
+      this.OidcManager.clearStaleState();
+      return user;
+    });
+  }
+
+  StartSignInWithRedirect() {
     let prmse = this.OidcManager.signinRedirect({}).then(args => false);
     return prmse;
   }
 
-  StartSignInWithPopUp(target: string) {
+  // simplest way is using pop up: doesn't have to remember original target, doesn't need separate HTML to capture hash fragment
+  StartSignInWithPopUp() {
     let prmse = this.OidcManager.signinPopup({}).then((user: User) => {
       if (!!user) {
         this.OidcUser = user;
